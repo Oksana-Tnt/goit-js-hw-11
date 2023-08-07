12036019bf55e4b2ec4da569388b718037a7eb05
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-08-08 00:37:06 +0200

    update style.css

--- src/templates/pixabay-markup.js
+++ src/templates/pixabay-markup.js
@@ -1,31 +1,33 @@
 function createMarkup(arr){
     return arr.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
-    `<div class="gallery">
-    
+    `<div class="photo-card"> 
     <a class="gallery__link" href="${largeImageURL}">
     <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
     </a>
     
     <div class="info">
       <p class="info-item">
-        <b>${likes}</b>
+        <b>Likes <br/>
+        ${likes}</b>
       </p>
 
       <p class="info-item">
-        <b>${views}</b>
+        <b>Views <br/>
+        ${views}</b>
       </p>
 
       <p class="info-item">
-        <b>${comments}</b>
+        <b>Comments <br/>
+        ${comments}</b>
       </p>
 
       <p class="info-item">
-        <b>${downloads}</b>
+        <b>Downloads <br/>
+        ${downloads}</b>
       </p>
       
-    </div>    
-
-  </div>`).join("");
+    </div>
+    </div>`).join(""); 
   
 };
 
