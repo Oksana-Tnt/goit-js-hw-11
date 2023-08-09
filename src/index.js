42d709e361ea677236c30d88b8f7a9fa4411daa2
Oksana-Tnt <127055817+Oksana-Tnt@users.noreply.github.com>
2023-08-09 17:22:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ let options = {
 };
 
 let observer = new IntersectionObserver(onLoad, options);
-lightbox =  new SimpleLightbox('.gallery a', { captionsData: "alt" ,captionDelay: 250, captionPosition: "bottom"});
+lightbox =  new SimpleLightbox(".gallery a", { captionsData: "alt" ,captionDelay: 250, captionPosition: "bottom"});
 
 function onSearch(evt) {   
 
